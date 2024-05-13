511ce2d39d5c1dfa6c8ca2705b8254e14a7e86b5
namsanghyeok <artexi1464.2@gmail.com>
2024-05-13 14:30:11 +0900

    add: task table 생성 migration sql

---
+++ src/main/resources/db/migration/V1__create_table.sql
@@ -0,0 +1,11 @@
+create table task
+(
+    task_id   bigint primary key auto_increment,
+    title     VARCHAR(50) not null,
+    content   text,
+    assignee  VARCHAR(30),
+    password  VARCHAR(30) not null,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
+    updated_at TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+    deleted_at TIMESTAMP // null 이 아니면 삭제됨
+);
