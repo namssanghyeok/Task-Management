1b9533db8ef43d7591470cb6cdb3c7c4264a4fc1
namsanghyeok <artexi1464.2@gmail.com>
2024-05-16 20:00:23 +0900

    add: FileValidator 테스트

---
+++ src/test/java/sparta/task/validation/validator/FileValidatorTest.java
@@ -0,0 +1,56 @@
+package sparta.task.validation.validator;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+import org.springframework.http.MediaType;
+import org.springframework.mock.web.MockMultipartFile;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+@ExtendWith(MockitoExtension.class)
+class FileValidatorTest {
+    private FileValidator fileValidator;
+
+    @BeforeEach
+    void beforeEach() {
+        this.fileValidator = new FileValidator();
+    }
+
+    @Test
+    @DisplayName("validation - 통과")
+    void isValid_success() {
+        // given
+        String contentType = MediaType.IMAGE_PNG_VALUE;
+        MockMultipartFile file = new MockMultipartFile("file", "test.txt", contentType, "Hello World".getBytes());
+        // when
+        boolean valid = fileValidator.isValid(file, null);
+        // then
+        assertThat(valid).isTrue();
+    }
+
+    @Test
+    @DisplayName("validation - 이미지 타입이 아니면 false")
+    void isValid_notImage() {
+        // given
+        String contentType = MediaType.TEXT_HTML_VALUE;
+        MockMultipartFile file = new MockMultipartFile("file", "test.txt", contentType, "Hello World".getBytes());
+        // when
+        boolean valid = fileValidator.isValid(file, null);
+        // then
+        assertThat(valid).isFalse();
+    }
+
+    @Test
+    @DisplayName("validation - 이미지 타입이 null 이면 false")
+    void isValid_null() {
+        // given
+        MockMultipartFile file = new MockMultipartFile("file", "test.txt", null, "Hello World".getBytes());
+        // when
+        boolean valid = fileValidator.isValid(file, null);
+        // then
+        assertThat(valid).isFalse();
+    }
+}
